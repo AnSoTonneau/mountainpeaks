1818fb0b788e5be5ab60fd138dd61b21fcbafcee
AnSoTonneau <annesophie.tonneau@pm.me>
2023-02-10 12:58:10 +0100

    creating database if doesn't exist

--- db.py
+++ db.py
@@ -1,11 +1,12 @@
 import databases
 import ormar
-import sqlalchemy
 from config import settings
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy_utils import database_exists, create_database
+import logging
 
 database = databases.Database(settings.db_url)
-metadata = sqlalchemy.MetaData()
-
+metadata = MetaData()
 
 class BaseMeta(ormar.ModelMeta):
     metadata = metadata
@@ -31,6 +32,12 @@ class MountainPeak(ormar.Model):
     altitude: float = ormar.Float(default=0.0, nullable=False)
 
 
-engine = sqlalchemy.create_engine(settings.db_url)
+engine = create_engine(settings.db_url)
+if not database_exists(engine.url):  # Checks for the first time
+    create_database(engine.url)  # Create new DB
+    logging.info("New Database Created" + str(database_exists(engine.url)))  # Verifies if database is there or not.
+else:
+    logging.info("Database Already Exists")
+
 metadata.drop_all(engine)
 metadata.create_all(engine)
